0db084d0e5c4e3051981faaba5f4a6e32c0c0e37
Moses Kamara <kamara.moses@yahoo.com>
2020-11-10 23:47:56 -0800

    update to the header/nav menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import { FaGithub } from 'react-icons/fa';
 import './style.css';
 
 // This is static top section of the page and gives it semantic value.
@@ -10,12 +9,11 @@ function Header() {
     return (
         <header className="header">
             <Container>
-                <Navbar className="justify-content-between">
+                <Navbar className="justify-content-center">
                     <Nav>
-                        <Nav.Link href="https://github.com/kamara-moses/UserDirectory" target="_Blank"><FaGithub className="github-icon" /></Nav.Link>
+                        <Nav><h1 className="text-center text-color">Team Directory</h1></Nav>
                     </Nav>
                 </Navbar>
-                <h1 className="text-center text-color">Team Directory</h1>
             </Container>
         </header>
     )
