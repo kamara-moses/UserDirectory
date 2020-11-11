f142d844265e5fd4f6678d8be5bc072f5dde1a8b
Moses Kamara <kamara.moses@yahoo.com>
2020-11-10 23:40:01 -0800

    update to teamcard style.css file

--- src/App.js
+++ src/App.js
@@ -158,11 +158,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-const sort = arr => arr.sort((a, b) => a - b);
-//By default,the sort() function sorts values as strings.Fix this by providing a compare function.
-// Example
-sort([1, 5, 2, 4, 3]);      // [1, 2, 3, 4, 5]
